ed4c06469306147831c502968f1e141d74b8e6dd
ksg-dev <ksg.dev.data@gmail.com>
2024-09-25 17:51:58 -0500

    add config file

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = os.environ["DB_URI"]
+
+
+class Base(DeclarativeBase):
+    pass
